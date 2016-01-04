d1197d5cbc54004b7ab93a0ce3a51c5971bdbb39
Scott Overholser <eigenholser@users.noreply.github.com>
2016-01-03 18:11:50 -0700

    Take command line argument for port.

--- test.sh
+++ test.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-curl localhost:3000/book | json_reformat
-curl localhost:3000/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json"
-curl localhost:3000/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json"
-curl localhost:3000/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json"
-curl localhost:3000/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json"
-curl localhost:3000/book | json_reformat
-curl localhost:3000/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json"
-curl localhost:3000/book | json_reformat
-curl localhost:3000/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json"
-curl localhost:3000/book | json_reformat
-curl localhost:3000/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json"
-curl localhost:3000/book | json_reformat
-curl localhost:3000/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json"
-curl localhost:3000/book | json_reformat
+port=$port
+if [ -z $port ]; then
+    port=3000
+fi
+
+curl localhost:$port/book | json_reformat
+curl localhost:$port/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json"
+curl localhost:$port/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json"
+curl localhost:$port/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json"
+curl localhost:$port/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json"
+curl localhost:$port/book | json_reformat
+curl localhost:$port/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json"
+curl localhost:$port/book | json_reformat
+curl localhost:$port/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json"
+curl localhost:$port/book | json_reformat
+curl localhost:$port/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json"
+curl localhost:$port/book | json_reformat
+curl localhost:$port/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json"
+curl localhost:$port/book | json_reformat
