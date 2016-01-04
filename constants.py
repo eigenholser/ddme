8ab7e12d08d538b2751bae0b53c28dbdf74edcf3
Scott Overholser <eigenholser@users.noreply.github.com>
2016-01-04 01:41:54 -0700

    Add docstring.

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+"""
+Constants here so we don't sprinkle them throughout the code.
+"""
+
 URL_PATH_BOOK = "/book"
 URL_NAME_BOOK = "book"
 URL_PATH_BUY = "/buy"
@@ -7,3 +11,4 @@ URL_NAME_SELL = "sell"
 HTTP_201_CREATED = 201
 HTTP_400_BAD_REQUEST = 400
 HTTP_500_INTERNAL_SERVER_ERROR = 500
+
