3edcd3140c2160274eaa1ff9750f809500326003
Scott Overholser <eigenholser@users.noreply.github.com>
2016-01-03 21:26:49 -0700

    Add quick and dirty verbose flag to curl commands.

--- test.sh
+++ test.sh
@@ -5,17 +5,21 @@ if [ -z $port ]; then
     port=3000
 fi
 
-curl localhost:$port/book | json_reformat
-curl localhost:$port/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json"
-curl localhost:$port/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json"
-curl localhost:$port/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json"
-curl localhost:$port/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json"
-curl localhost:$port/book | json_reformat
-curl localhost:$port/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json"
-curl localhost:$port/book | json_reformat
-curl localhost:$port/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json"
-curl localhost:$port/book | json_reformat
-curl localhost:$port/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json"
-curl localhost:$port/book | json_reformat
-curl localhost:$port/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json"
-curl localhost:$port/book | json_reformat
+if [ ! -z $2 ]; then
+    verbose="-v"
+fi
+
+curl localhost:$port/book $verbose | json_reformat
+curl localhost:$port/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/book $verbose | json_reformat
+curl localhost:$port/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/book $verbose | json_reformat
+curl localhost:$port/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/book $verbose | json_reformat
+curl localhost:$port/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/book $verbose | json_reformat
+curl localhost:$port/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json" $verbose
+curl localhost:$port/book $verbose | json_reformat
