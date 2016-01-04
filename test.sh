8df27d2fcc3a96cd0dc3163c8dea397daf337b7e
Scott Overholser <eigenholser@users.noreply.github.com>
2016-01-03 22:06:06 -0700

    Add formatting to validation shell script.

--- test.sh
+++ test.sh
@@ -9,17 +9,53 @@ if [ ! -z $2 ]; then
     verbose="-v"
 fi
 
-curl localhost:$port/book $verbose | json_reformat
-curl localhost:$port/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/book $verbose | json_reformat
-curl localhost:$port/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/book $verbose | json_reformat
-curl localhost:$port/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/book $verbose | json_reformat
-curl localhost:$port/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/book $verbose | json_reformat
-curl localhost:$port/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json" $verbose
-curl localhost:$port/book $verbose | json_reformat
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
+echo "***"
+curl --silent localhost:$port/sell --data '{"qty":10,"prc":15}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/sell --data '{"qty":10,"prc":13}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/buy  --data '{"qty":10,"prc":7}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/buy  --data '{"qty":10,"prc":9.5}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
+echo "***"
+curl --silent localhost:$port/sell --data '{"qty":5, "prc":9.5}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
+echo "***"
+curl --silent localhost:$port/buy  --data '{"qty":6, "prc":13}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
+echo "***"
+curl --silent localhost:$port/sell --data '{"qty":7, "prc":7}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
+echo "***"
+curl --silent localhost:$port/sell --data '{"qty":12, "prc":6}' -H "Content-Type: application/json" $verbose
+echo
+echo
+echo "***"
+curl --silent localhost:$port/book $verbose | json_reformat
+echo
